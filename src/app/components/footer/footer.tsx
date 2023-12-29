7972c30ff8c918b9984897711f60ebd4f8ceffc2
Adrian Maj <adrianmaj1122@gmail.com>
2023-12-29 13:29:33 +0100

    Fixed header, changed footer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -3,12 +3,18 @@ import classes from './footer.module.scss'
 import Link from 'next/link'
 
 const Footer = () => {
+	const date = new Date()
+	const year = date.getFullYear()
 	return (
 		<footer className={classes.footer}>
 			<div className={classes.background}></div>
 			<Link href="/">
 				<img src="./logoMono.svg" className={classes.logo} alt="PUMPFIT Logo" />
 			</Link>
+			<div className={classes.attribution}>
+				<div className={classes.socials}></div>
+				<p className={classes.attributionText}>&copy; {year} PUMPFIT Company</p>
+			</div>
 		</footer>
 	)
 }
