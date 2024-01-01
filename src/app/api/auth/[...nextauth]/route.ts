7a0b86ac6ad5bec1b287a6357fc2cad7b71b7bc0
Adrian Maj <adrianmaj1122@gmail.com>
2024-01-01 19:35:58 +0100

    Fixed build

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,33 @@
-import { PrismaAdapter } from '@auth/prisma-adapter'
-import NextAuth from 'next-auth'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import prisma from '../../../../../lib/prisma'
+// import { PrismaAdapter } from '@auth/prisma-adapter'
+// import NextAuth from 'next-auth'
+// import CredentialsProvider from 'next-auth/providers/credentials'
+// import prisma from '../../../../../lib/prisma'
 
-declare module '@auth/core/types' {
-	interface User {}
-}
+// declare module '@auth/core/types' {
+// 	interface User {}
+// }
 
-const handler = NextAuth({
-	providers: [
-		CredentialsProvider({
-			async authorize(credentials: string) {
-				const authResponse = await fetch('/users/login', {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-					},
-					body: JSON.stringify(credentials),
-				})
+// const handler = NextAuth({
+// 	providers: [
+// 		CredentialsProvider({
+// 			async authorize(credentials: string) {
+// 				const authResponse = await fetch('/users/login', {
+// 					method: 'POST',
+// 					headers: {
+// 						'Content-Type': 'application/json',
+// 					},
+// 					body: JSON.stringify(credentials),
+// 				})
 
-				if (!authResponse.ok) {
-					return null
-				}
+// 				if (!authResponse.ok) {
+// 					return null
+// 				}
 
-				const user = await authResponse.json()
+// 				const user = await authResponse.json()
 
-				return user
-			},
-		}),
-	],
-})
-export { handler as GET, handler as POST }
+// 				return user
+// 			},
+// 		}),
+// 	],
+// })
+// export { handler as GET, handler as POST }
