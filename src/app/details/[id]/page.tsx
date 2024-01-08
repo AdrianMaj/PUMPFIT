e1802837c01c21b6d2e8412abf3102f32abf9cc9
Adrian Maj <adrianmaj1122@gmail.com>
2024-01-08 22:57:17 +0100

    Fixed type error

--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -1,17 +1,18 @@
 import DetailsSection from '@/components/details/detailsSection'
 import Footer from '@/components/footer/footer'
 import Header from '@/components/navbar/header'
-import { Announcement } from '@/types/databaseTypes'
+import { Announcement, Trainer } from '@/types/databaseTypes'
 import fetchTrainerData from '@/util/fetchTrainerData'
 import React from 'react'
 const Page = async ({ params }: { params: { id: string } }) => {
 	const account = await fetchTrainerData(params.id)
 	if (account && account.trainer && account.trainer.announcement) {
-		const annoucement = account.trainer.announcement as Announcement
+		const trainer = account.trainer as Trainer
+		const announcement = trainer.announcement as Announcement
 		return (
 			<>
 				<Header />
-				<DetailsSection trainerName={account.name} trainerData={annoucement} id={params.id} />
+				<DetailsSection trainerName={account.name} trainerData={announcement} id={params.id} />
 				<Footer />
 			</>
 		)
