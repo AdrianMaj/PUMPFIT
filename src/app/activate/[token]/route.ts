b4db29850c62357a262ac8fbf3a0c22d3e95266a
Adrian Maj <adrianmaj1122@gmail.com>
2024-01-17 10:25:54 +0100

    Added email verification logic

---
+++ src/app/activate/[token]/route.ts
@@ -0,0 +1,52 @@
+import { NextRequest } from 'next/server'
+import prisma from '../../../../lib/prisma'
+import { redirect } from 'next/navigation'
+
+const GET = async (request: NextRequest, { params }: { params: { token: string } }) => {
+	const { token } = params
+	const account = await prisma.account.findFirst({
+		where: {
+			ActivateToken:{
+                some:{
+                    AND: [
+                        {
+                            activatedAt: null;
+                        },
+                        {
+                            createdAt: {
+                                gt: new Date(Date.now() - 24 * 60 * 60 * 1000) //24 hrs
+                            }
+                        },
+                        {
+                            token
+                        }
+                    ]
+                }
+            }
+		},
+	})
+    
+    if(!account){
+        throw new Error('Invalid token')
+    }
+
+    await prisma.account.update({
+        where: {
+            id: account.id
+        },
+        data: {
+            active: true;
+        }
+    })
+
+    await prisma.activateToken.update({
+        where: {
+            token
+        },
+        data:{
+            activatedAt: new Date(),
+        }
+    })
+    
+    redirect('/login')
+}
