8e0f7c9acb2fd77bec68baa4c14e743f85ed0eaf
Adrian Maj <adrianmaj1122@gmail.com>
2024-02-08 14:47:39 +0100

    Added cloudinary support

--- src/util/uploadFiles.ts
+++ src/util/uploadFiles.ts
@@ -6,19 +6,22 @@ const uploadFiles = async (files: File[]) => {
 	const fileUrls: string[] = []
 	const cloudName = process.env.CLOUDINARY_CLOUD_NAME
 
-	files.forEach(async file => {
+	for (const file of files) {
 		const formData = new FormData()
 		formData.append('file', file)
 		formData.append('upload_preset', 'pumpfit')
 		try {
 			const response = await axios.post(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, formData)
 			console.log(response)
+
+			const result = await cloudinary.uploader.upload(response.data.secure_url)
+			fileUrls.push(result.secure_url)
 		} catch (error) {
 			console.error(error)
 		}
-		const result = await cloudinary.uploader.upload(file.name)
-		fileUrls.push(result.secure_url)
-	})
+	}
+
+	return fileUrls
 }
 
 export default uploadFiles
